989e83f4595e958abc04f8a73e28f6b0dc525b28
imran00852 <bhatimran9797@gmail.com>
2024-11-23 14:43:54 +0530

    added readme

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { newTask, getAllTasks,updateTask,deleteTask } from "../controllers/taskControllers.js";
+import {
+  newTask,
+  getAllTasks,
+  updateTask,
+  deleteTask,
+} from "../controllers/taskControllers.js";
 
 const router = Router();
 
 router.post("/new", isAuthenticated, newTask);
 router.get("/all", isAuthenticated, getAllTasks);
-router.route("/:id").put(updateTask).delete(deleteTask);
+router
+  .route("/:id")
+  .put(isAuthenticated, updateTask)
+  .delete(isAuthenticated, deleteTask);
 
 export default router;
